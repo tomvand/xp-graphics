bb6e95f0c02baa4143c27da245182cecf6ec714f
Tom van Dijk <tomvand@users.noreply.github.com>
2021-01-06 11:34:59 +0100

    Retune vis-lights for default (non-offset) textures and adapt cars lod

--- vis-lights.lua
+++ vis-lights.lua
@@ -4,7 +4,7 @@ function vis_lights()
     if os.clock() > start_time then
         -- Tuned for TK Ligh Mod 2019
         -- https://forums.x-plane.org/index.php?/files/file/47394-tk-light-mod-2019/
-        -- With X-Lights 1.0.1 **only 1000_lights_close.dds**
+        -- With default light textures
         --
         -- This script adapts light brightness (exponent) based on sun elevation for more gradual transition.
         
@@ -12,6 +12,8 @@ function vis_lights()
         set("sim/private/controls/lights/hdr_mix", 0.0) -- Light blob intensity, 0=bright, 1=invisible
         set("sim/private/controls/lights/dist_near", 200.0) -- Near side of exponent transition
         set("sim/private/controls/lights/dist_far", 10000.0) -- Far side of exponent transition
+        set("sim/private/controls/lights/scale_far", 0.8) -- Increase distant light blob size (default: 0.67)
+        -- set("sim/private/controls/cars/lod_min", 60000) -- FPS heavy, cars further in distance, adds a lot visually
 
         -- Inputs
         local sun_pitch = get("sim/graphics/scenery/sun_pitch_degrees")
@@ -28,8 +30,8 @@ function vis_lights()
         if night_ratio > 1.0 then
             night_ratio = 1.0
         end
-        local exp_near = (1.0 - night_ratio) * 0.38 + night_ratio * 0.42 -- day: 0.38 (default), night: 0.42
-        local exp_far  = (1.0 - night_ratio) * 0.42 + night_ratio * 0.50 -- day: 0.42,           night: 0.50 (very bright)
+        local exp_near = (1.0 - night_ratio) * 0.38 + night_ratio * 0.45 -- day: 0.38 (default), night: 0.45
+        local exp_far  = (1.0 - night_ratio) * 0.42 + night_ratio * 0.53 -- day: 0.42,           night: 0.53 (very bright)
 
         -- Fog/visibility
         -- local fog_ratio = 1.0 - (visibility - 2000) / (7000 - 2000) -- Transition from 7000m (0.0) to 2000m (1.0)
@@ -53,8 +55,8 @@ function vis_lights()
         elseif exp_far_now > exp_far then
             exp_far_now = exp_far_now - 0.01 * frametime
         end
-        set("sim/private/controls/lights/exponent_near", exp_near_now)
-        set("sim/private/controls/lights/exponent_far", exp_far_now)
+        set("sim/private/controls/lights/exponent_near", exp_near_now)  -- default: 0.41
+        set("sim/private/controls/lights/exponent_far", exp_far_now)    -- default: 0.45
     end
 end
 
